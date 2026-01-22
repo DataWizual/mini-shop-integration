7145b96f54e1a287cdc93ece73549aa13d68aac0
Data Wizual <121548721+DataWizual@users.noreply.github.com>
2026-01-22 12:21:38 +0500

    Create test.sol

---
+++ test.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract VulnerableBank {
+    mapping(address => uint) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        uint bal = balances[msg.sender];
+        require(bal > 0);
+
+        // УЯЗВИМОСТЬ: Сначала отправка денег, потом смена баланса
+        (bool sent, ) = msg.sender.call{value: bal}("");
+        require(sent, "Failed to send Ether");
+
+        balances[msg.sender] = 0;
+    }
+}
